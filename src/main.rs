52554a3877ab575a284aafbb4fb153a3c090b8a9
markussigurbjornsson <markussigurbjorns@gmail.com>
2024-12-16 23:50:49 +0100

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use axum::extract::{DefaultBodyLimit, Multipart, Path, State};
-use axum::{routing::get, routing::post, Router};
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    routing::post,
+    Router,
+};
 use std::fs::{self, File};
 use std::io::Write;
 use std::path::PathBuf;
@@ -7,35 +13,58 @@ use std::sync::Arc;
 use tokio::sync::Mutex;
 use tower_http::cors::{Any, CorsLayer};
 use tower_http::limit::RequestBodyLimitLayer;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    //directory to create
-    let storage_dir = Arc::new(Mutex::new("uploads".to_string()));
-    fs::create_dir_all(&*storage_dir.lock().await).unwrap();
+    let frontend = async {
+        // Serve static files
+        let not_found_service = Router::new().route(
+            "/*path",
+            get(handler_404), // convert handler to a GET route
+        );
 
-    // Define CORS layer
-    // Todo: Only allow curate oslo domain in the future
-    let cors = CorsLayer::new().allow_origin(Any).allow_methods(Any);
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:6868").await.unwrap();
+        let app = Router::new().nest_service(
+            "/",
+            ServeDir::new("public").not_found_service(not_found_service),
+        );
+        axum::serve(listener, app).await.unwrap();
+    };
 
-    //set up routes
-    let app = Router::new()
-        .route(
-            "/upload",
-            post(upload_file_handler).layer(RequestBodyLimitLayer::new(300_000_000)),
-        )
-        .route("/download/:filename", get(download_file_handler))
-        .route("/play/:filename", get(play_audio_file_handler))
-        .layer(DefaultBodyLimit::disable())
-        .with_state(storage_dir.clone())
-        .layer(cors);
+    let backend = async {
+        // Directory to create
+        let storage_dir = Arc::new(Mutex::new("uploads".to_string()));
+        fs::create_dir_all(&*storage_dir.lock().await).unwrap();
 
-    //run server
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:6969").await.unwrap();
-    println!("Listening on 0.0.0.0:6969");
-    axum::serve(listener, app).await.unwrap();
+        // Define CORS layer
+        // Todo: Only allow curate oslo domain in the future
+        let cors = CorsLayer::new().allow_origin(Any).allow_methods(Any);
+
+        // Set up routes
+        let app = Router::new()
+            .route(
+                "/upload",
+                post(upload_file_handler).layer(RequestBodyLimitLayer::new(300_000_000)),
+            )
+            .route("/download/:filename", get(download_file_handler))
+            .route("/play/:filename", get(play_audio_file_handler))
+            .layer(DefaultBodyLimit::disable())
+            .with_state(storage_dir.clone())
+            .layer(cors);
+
+        //run server
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:6969").await.unwrap();
+        axum::serve(listener, app).await.unwrap();
+    };
+
+    println!("Listening on 0.0.0.0:6868");
+    tokio::join!(frontend, backend);
 }
-
+async fn handler_404() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND,
+            Html("<!DOCTYPE html><html><head><title>404</title></head><body><h1>nothing to see here</h1></body></html>"))
+}
 async fn upload_file_handler(
     State(storage_dir): State<Arc<Mutex<String>>>,
     mut multipart: Multipart,
@@ -65,7 +94,7 @@ async fn download_file_handler(
         let file = tokio::fs::File::open(file_path)
             .await
             .map_err(|_| axum::http::StatusCode::INTERNAL_SERVER_ERROR)?;
-        
+
         let stream = tokio_util::io::ReaderStream::new(file);
         let body = axum::body::Body::from_stream(stream);
 
